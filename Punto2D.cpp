0a67e55e699a8ed9e211453f53e742b47175bcd3
Federico Bruzone <federicobruzone@yahoo.com.ar>
2015-09-09 19:11:49 -0300

    Corregido error de retorno de LeerPunto2D

--- Punto2D.cpp
+++ Punto2D.cpp
@@ -4,9 +4,9 @@ Punto2D p;
 
 istream& LeerPunto2D(Punto2D &p) 
 {
-	cin >> p.posx >> p.posy;
-}
+	return cin >> p.posx >> p.posy;
+};
 
 void MostrarPunto2D (Punto2D &p){
 	cout << "" << endl << p.posx  << ""  << endl << p.posy ;
-}
+};
